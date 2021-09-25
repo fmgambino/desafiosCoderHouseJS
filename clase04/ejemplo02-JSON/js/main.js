bb51446e24b045dd247b6604280f5b0f9a117543
fmgambino <fernando.m.gambino@gmail.com>
2021-09-25 09:50:52 -0300

    Se agrega Clase 05 JS

--- clase04/ejemplo02-JSON/js/main.js
+++ clase04/ejemplo02-JSON/js/main.js
@@ -1,12 +1,34 @@
 //ALMACENAR OBJETOS EN EL STORAGE
 
-const producto1 = '{"id":2, producto: "Arroz"}';
+/*const producto1 = '{"id":2, producto: "Arroz"}';
 //localStorage.setItem("producto1", producto1); // Se guarda [object object]
-const enJSON = JSON.stringify (enJSON);
+const enJSON = JSON.stringify (producto1);
 
 console.log(enJSON);
 console.log(typeof producto1);
 console.log(typeof enJSON);
 
 localStorage.setItem("producto1", enJSON);
-// se guarda {"id", "producto": "Arroz"}
+// se guarda {"id", "producto": "Arroz"} */
+
+//EJEMPLO: OBTENER ARRAY ALMACENADO
+
+class Producto {
+    constructor(obj){
+        this.nombre = obj.producto.toUpperCase();
+        this.precio = parseFloat(objeto.precio);
+    }
+    sumaIVA(){
+        this.precio = this.precio * 1.21;
+    }
+}
+
+//Obtenemos el listado de los productos almacenado
+const almacenados = JSON.parse(localStorage.getItem("listaProductos"));
+const productos = [];
+//Iteramos almacenadis con for...of
+for(const objeto of almacenados)
+    productos.push(new Producto(objeto));
+//Ahora tenemos objetos
+for (const producto of productos)
+    producto.sumaIVA();  
